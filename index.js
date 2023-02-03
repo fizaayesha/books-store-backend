67b8bda149dfe69dcb67f00cb02913f13b685dd6
fizaayesha <fizaayesha696@gmail.com>
2023-02-04 00:19:01 +0530

    new code added

--- index.js
+++ index.js
@@ -25,22 +25,47 @@ app.get("/books", (req, res) => {
 
 app.post("/books", (req, res) => {
   const q =
-    "INSERT INTO books (`id`,`title`, `description`, `price`, `cover`) VALUES (?)";
+    "INSERT INTO books (`title`, `description`, `price`, `cover`) VALUES (?)";
   const values = [
     // "title from backend",
     // "desc from backend",
     // "cover picture from backend",o
-    req.body.id,
+    // req.body.id,
     req.body.title,
     req.body.description,
     req.body.price,
     req.body.cover,
   ];
   db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book added");
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Book added");
   });
 });
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title` =?, `desc`=?, `price`=?, `cover`=? WHERE id=?";
+  const values=[
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ]
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Book has been updated successfully");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Boo has been deleted successfully");
+  });
+});
+
 app.listen(8000, () => {
   console.log("Connected to backend");
 });
